2e4fffcaa92e3eb04b12b5d1e25f489494810cb9
mwave <lorin@public-action.org>
2017-10-24 13:27:56 -0500

    added 2 new feeds. staggered the timer method to run 5 minutes apart

--- src/main/java/org/pa/ejb/RssTimer.java
+++ src/main/java/org/pa/ejb/RssTimer.java
@@ -11,8 +11,9 @@ import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import org.pa.rss.FeedTypes;
 import org.pa.rss.RemoteFeed;
+import org.pa.rss.data.CoopNewsFeedData;
 import org.pa.rss.data.DemAtWorkFeedData;
-import org.pa.rss.data.DemEnterpriseFeedData;
+import org.pa.rss.data.DemCollabFeedData;
 import org.pa.rss.data.GoogleFeedData;
 import org.pa.rss.data.RedditFeedData;
 import org.pa.rss.data.UsFedCoopFeedData;
@@ -38,7 +39,7 @@ public class RssTimer {
     }
 
     @Asynchronous
-    @Schedule(hour = "*/12", minute = "0", second = "0", persistent = false)
+    @Schedule(hour = "*/12", minute = "5", second = "0", persistent = false)
     public void usFedWorkerCoopRssTimer() {
         try {
             RemoteFeed rf = new RemoteFeed(FeedTypes.US_FED_COOP_RSS_URL);
@@ -50,21 +51,9 @@ public class RssTimer {
         }
     }
 
-    @Asynchronous
-    @Schedule(hour = "*/12", minute = "0", second = "0", persistent = false)
-    public void demEnterpriseRssTimer() {
-        try {
-            RemoteFeed rf = new RemoteFeed(FeedTypes.DEMOC_ENTERPISE_RSS_URL);
-            DemEnterpriseFeedData.getInstance().setNodeList(rf.getData());
-        } catch (ConcurrentAccessTimeoutException cae) {
-
-        } catch (Exception une) {
-
-        }
-    }
 
     @Asynchronous
-    @Schedule(hour = "*/12", minute = "0", second = "0", persistent = false)
+    @Schedule(hour = "*/12", minute = "10", second = "0", persistent = false)
     public void redditRssTimer() {
         try {
             RemoteFeed rf = new RemoteFeed(FeedTypes.REDDIT_RSS_URL, FeedTypes.ATOM);
@@ -77,11 +66,37 @@ public class RssTimer {
     }
 
     @Asynchronous
-    @Schedule(hour = "*/12", minute = "0", second = "0", persistent = false)
+    @Schedule(hour = "*/12", minute = "15", second = "0", persistent = false)
     public void googleRssTimer() {
         try {
             RemoteFeed rf = new RemoteFeed(FeedTypes.GOOGLE_RSS_URL);
             GoogleFeedData.getInstance().setNodeList(rf.getData());
+        } catch (ConcurrentAccessTimeoutException cae) {
+
+        } catch (Exception une) {
+
+        }
+    }
+    
+    @Asynchronous
+    @Schedule(hour = "*/12", minute = "20", second = "0", persistent = false)
+    public void coopNewsRssTimer() {
+        try {
+            RemoteFeed rf = new RemoteFeed(FeedTypes.COOP_NEWS_RSS_URL);
+            CoopNewsFeedData.getInstance().setNodeList(rf.getData());
+        } catch (ConcurrentAccessTimeoutException cae) {
+
+        } catch (Exception une) {
+
+        }
+    }
+    
+    @Asynchronous
+    @Schedule(hour = "*/12", minute = "25", second = "0", persistent = false)
+    public void demCollabRssTimer() {
+        try {
+            RemoteFeed rf = new RemoteFeed(FeedTypes.DEM_COLLAB_RSS_URL);
+            DemCollabFeedData.getInstance().setNodeList(rf.getData());
         } catch (ConcurrentAccessTimeoutException cae) {
 
         } catch (Exception une) {
